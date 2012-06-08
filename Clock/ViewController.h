fb726e9abb947c3fa0be4df2287727888d0526da
Alexis Kinsella <alexis.kinsella@gmail.com>
2012-06-09 00:13:36 +0200

    Initial Commit

---
+++ Clock/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Clock
+//
+//  Created by Alexis Kinsella on 09/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ClockTests/ClockTests.m
@@ -0,0 +1,32 @@
+//
+//  ClockTests.m
+//  ClockTests
+//
+//  Created by Alexis Kinsella on 09/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ClockTests.h"
+
+@implementation ClockTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ClockTests");
+}
+
+@end
